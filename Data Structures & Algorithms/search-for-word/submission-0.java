2065bc95bd0f07a8addb0ff7dc995d813b3f7b24
Tejsvapandey1 <142985566+Tejsvapandey1@users.noreply.github.com>
2026-07-15 09:37:36 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, i, j, 0))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] board, String word, int r, int c, int idx) {
+        // Entire word matched
+        if (idx == word.length())
+            return true;
+
+        // Out of bounds or mismatch
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length)
+            return false;
+
+        if (board[r][c] != word.charAt(idx))
+            return false;
+
+        // Mark visited
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = dfs(board, word, r + 1, c, idx + 1) || dfs(board, word, r - 1, c, idx + 1)
+            || dfs(board, word, r, c + 1, idx + 1) || dfs(board, word, r, c - 1, idx + 1);
+
+        // Backtrack
+        board[r][c] = temp;
+
+        return found;
+    }
+}
